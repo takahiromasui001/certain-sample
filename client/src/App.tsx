e2bd777f1f6dee9010ca54edc69bcf2880262f5f
Takahiro Masui <takahiro.masui@luxiar.com>
2020-05-03 16:12:43 +0900

    [client] yarn add react-router-dom & ルーティング動作確認

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ProductList from './ProductList'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
 
   return (
-    <ProductList/>
+    <Router>
+      <Switch>
+        <Route path="/products">
+          <ProductList/>
+        </Route>
+        <Route path="/">
+          <div>Test</div>
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
