38cf1a7d97b00d7be2d7ed43fa791ecee31d21f4
Takahiro Masui <takahiro.masui@luxiar.com>
2020-05-05 11:36:54 +0900

    [client] Productラベル修正

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'products/index'
   namespace 'api' do
     namespace 'v1' do
-      resources :products
+      resources :products, only: [:index, :create, :show, :update, :destroy]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
