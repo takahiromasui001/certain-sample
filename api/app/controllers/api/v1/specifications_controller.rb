63a3975be64d32c120f622b0044f12c8b96e6d68
Takahiro Masui <takahiro.masui@luxiar.com>
2020-05-05 11:54:57 +0900

    [api] Specificationのエンドポイントまで

---
+++ api/app/controllers/api/v1/specifications_controller.rb
@@ -0,0 +1,22 @@
+module Api
+  module V1
+    class SpecificationsController < ApplicationController
+      # GET	/api/v1/specifications/:id
+      def show
+        specification = Specification.find(params[:id])
+
+        # HACK: controllerではなくSerializerに書くべき内容
+        specification_items = specification.specification_items.map { |item| 
+          {
+            name: item.name, type: item.specification_type, product_name: item.product.name, maker: item.product.maker
+          }
+        }
+        response = {
+          name: specification.name,
+          specification_items: specification_items
+        }
+        render json: response
+      end
+    end
+  end
+end
