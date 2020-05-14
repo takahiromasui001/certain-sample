bea233488626873b248451d06a8cf41e7938a918
Takahiro Masui <takahiro.masui@luxiar.com>
2020-05-14 23:42:13 +0900

    [api] Specificationにstatus/construction_methodを追加

--- api/app/models/specification.rb
+++ api/app/models/specification.rb
@@ -1,3 +1,6 @@
 class Specification < ApplicationRecord
   has_many :specification_items
+
+  enum status: { start: 0, completed: 1 }
+  enum construction_method: { conventional: 0 }
 end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_132820) do
+ActiveRecord::Schema.define(version: 2020_05_14_140618) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 2020_05_04_132820) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
+    t.integer "amount"
+    t.integer "construction_method"
   end
 
   add_foreign_key "specification_items", "products"
