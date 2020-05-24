4d85752a9296a82455f4bfe9f06e3505201c5fa7
Takahiro Masui <takahiro.masui@luxiar.com>
2020-05-24 12:30:46 +0900

    [api] カスタマイズ要望 カスタマイズ要望の登録処理

--- api/app/controllers/api/v1/specification_items_controller.rb
+++ api/app/controllers/api/v1/specification_items_controller.rb
@@ -29,10 +29,30 @@ module Api
           name: params[:name], specification_type: params[:type], 
           product_id: params[:productId], color_id: params[:colorId]
         )
+
+        new_candidate_instances, delete_candidate_ids = createCandidateIds(item)
+        CandidateProduct.import(new_candidate_instances)
+        CandidateProduct.destroy(delete_candidate_ids)
+
+        pp item.candidates
         render json: build_response(item)
       end
 
       private
+
+      def createCandidateIds(item)
+        current_candidate_ids = item.candidates.ids
+        next_candidate_ids = params[:productCandidate]
+
+        new_candidate_ids = next_candidate_ids.filter { |i| current_candidate_ids.exclude?(i) }
+        new_candidate_instances = []
+        new_candidate_ids.each { |i| new_candidate_instances << item.candidate_products.build(product_id: i) }
+
+        delete_product_ids = current_candidate_ids.filter { |i| next_candidate_ids.exclude?(i) }
+        delete_candidate_ids = CandidateProduct.where(product_id: delete_product_ids).ids
+
+        [new_candidate_instances, delete_candidate_ids]
+      end
 
       def build_response(item)
         {
