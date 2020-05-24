9635ebbfc1ca2c91230b86b77091a6521b3662b4
Takahiro Masui <takahiro.masui@luxiar.com>
2020-05-24 10:37:05 +0900

    [api] カスタマイズ要望 candidate_products(中間テーブルの作成)

--- api/app/models/product.rb
+++ api/app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_many :specification_items
   has_many :colors
+  has_many :candidate_products
 end
--- api/app/models/specification_item.rb
+++ api/app/models/specification_item.rb
@@ -2,6 +2,8 @@ class SpecificationItem < ApplicationRecord
   belongs_to :specification
   belongs_to :product, optional: true
   belongs_to :color, optional: true
+  has_many :candidate_products
+  has_many :candidates, through: :candidate_products, source: :product
 
   enum specification_type: { inner: 0, outer: 1, inner_finishing: 2, equipment: 3 }
 end
